a3955a2f7476df454e2eddbcb918ae2d9baec6b0
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2022-09-20 03:06:40 +0000

    customer up

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :customer.post_number, :customer.phone_number, :customer.address, :customer.email, :customer.is_valid)
   end
 end
