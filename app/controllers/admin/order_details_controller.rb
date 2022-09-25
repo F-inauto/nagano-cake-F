12ad086cd00e0ce62ab1089b8a00c5746f5bc1cc
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2022-09-25 13:19:18 +0000

    order up

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,16 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    @order_details = Order_detais.find(order_details_params)
+    @order_details = OrderDetais.find(params[:id])
     if @order_details.update(order_details_params) &&  @order_details.making_status == "making"
       #製作ステータス製作中→製作中
       @order_details.order.update_all(order_status: 2)
       redirect_to admin_order_path(@order_details.id)
+    elsif @order_details.update(order_details_params) && @order_details.making_status == "complete"
+      @order_details.order.update_all(order_status: 3)
+      redirect_to admin_order_path(@order_details.id)
+    else
+      render 'index'
     end
   end
 
