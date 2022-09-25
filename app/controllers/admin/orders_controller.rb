220f276c029410c0e58cf8b581d8dea09bdcaf4d
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2022-09-25 10:06:41 +0000

    order up

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -22,15 +22,14 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     if @order.update(order_params)
        flash[:notice] = "注文ステータス更新"
-       #入金確認→製作ステータス：製作待ち
-      @order.order_details.update_all(making_status: 1) if @order.order_status == "confirm_payment"
-       #製作ステータス製作中→製作中
-      # @order.update_all(making_status: 2) if @order.order_details.order_status == "making"
-      #製作完了→発送準備
-      # @order.update_all(order_status: 3) if @order.order_details.making_status == "complete"
-      redirect_to admin_order_path(@order.id)
+       @order.order_details.update_all(order_status: 0) if order.order_status == "wait_payment"
+       @order.update_all(order_status: 1) if order.order_details.order_status == "confirm_payment"
+       @order.update_all(order_status: 2) if order.order_details.order_status == "making"
+       @order.update_all(order_status: 3) if order.order_details.order_status == "preparing_ship"
+       @order.update_all(order_status: 4) if order.order_details.order_status == "finish_prepare:"
+       redirect_back(fallback_location: root_path)
     else
-      redirect_to admin_order_path(@order.id)
+      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -39,6 +38,6 @@ class Admin::OrdersController < ApplicationController
 
   private
     def order_params
-      params.require(:order).permit(:customer_id, :post_number, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status, :amount)
+      params.require(:item).permit(:customer_id, :post_number, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status, :amount)
     end
 end
