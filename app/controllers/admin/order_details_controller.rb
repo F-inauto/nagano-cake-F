465f73d4001339f7fa8de66cdd673e0992d18d53
Ayaka1988 <109730343+Ayaka1988@users.noreply.github.com>
2022-09-25 08:31:45 +0000

    admin_order

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
 
+  before_action :authenticate_admin!
+
   def update
-    @order_details = Order_detais.find(order_details_params)
-    @order_details.update
+    @order_details = OrderDetais.find(params[:id])
+    @order = @order_detail.order
+    @order_details.update(order_detail_params)
+
+
+
   end
 end
