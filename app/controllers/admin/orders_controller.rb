6247472748f4b3f769222ae96a938cbe2f5f6791
Ayaka1988 <109730343+Ayaka1988@users.noreply.github.com>
2022-09-25 05:12:01 +0000

    admin_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,7 +5,7 @@ class Admin::OrdersController < ApplicationController
     # @order = Order.find(params[:id])
   end
 
-  def crate
+  def create
     @order = Order.new(order_params)
     @order.save
   end
@@ -18,13 +18,26 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
-  def update
+  def order_status_update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+       flash[:notice] = "注文ステータス更新"
+       @order.order_details.update_all(order_status: 0) if order.order_status == "wait_payment"
+       @order.update_all(order_status: 1) if order.order_details.order_status == "confirm_payment"
+       @order.update_all(order_status: 2) if order.order_details.order_status == "making"
+       @order.update_all(order_status: 3) if order.order_details.order_status == "preparing_ship"
+       @order.update_all(order_status: 4) if order.order_details.order_status == "finish_prepare:"
+       redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
+
+
+
 
   private
     def order_params
       params.require(:item).permit(:customer_id, :post_number, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status, :amount)
     end
-
-
 end
